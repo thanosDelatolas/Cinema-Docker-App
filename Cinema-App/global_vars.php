5f87218a135b855bf3a24bd35ef3bcbb6a1a2e54
thanosdel <thanos.delatolas98@gmail.com>
2020-11-10 13:25:58 +0200

    nice

---
+++ Cinema-App/global_vars.php
@@ -0,0 +1,9 @@
+<?php
+
+    // ********************* GLOBAL VARIABLES *********************
+    $GLOBALS['client_id'] = "7ffbed02-ac0c-4784-8c52-2cf8d77f85fd";
+    $GLOBALS['client_secret'] = "1dacab95-4511-49bf-ba39-80119f37b094";
+    $GLOBALS['redirect_uri']= "http://localhost:4000/welcome.php";
+    $GLOBALS['auth_url'] ="http://172.18.1.5:3000/oauth2/authorize";
+    $GLOBALS['access_token_url'] ="http://172.18.1.5:3000/oauth2/token";
+?>
--- Cinema-App/login.php
+++ Cinema-App/login.php
@@ -2,19 +2,14 @@
   /*
   * redirect to keyrock login!!!
   */
-   $client_id = "7ffbed02-ac0c-4784-8c52-2cf8d77f85fd";
-   $client_secret = "1dacab95-4511-49bf-ba39-80119f37b094";
-
-   $auth_url = "http://172.18.1.5:3000/oauth2/authorize";
-
-   $redirect_uri ="http://localhost:4000/welcome.php";
- 
+  
+   require 'global_vars.php';
       
    $keyrock_url = $auth_url."?". http_build_query([
-      'client_id' => $client_id,
+      'client_id' =>  $GLOBALS['client_id'],
       'response_type' => 'code',
       'state' => 'get_code',
-      'redirect_uri' => $redirect_uri,     
+      'redirect_uri' => $GLOBALS['redirect_uri'],     
    ]);
    header("location: ".$keyrock_url); 
 
