be03e905becec50bca41e14703ad58fde0dd1371
thanosdel <thanos.delatolas98@gmail.com>
2020-11-12 18:12:06 +0200

    mongo db finalyy!

--- Data-Storage/mongo_conn.php
+++ Data-Storage/mongo_conn.php
@@ -1,4 +1,21 @@
 <?php
-    $connection = new MongoClient(); // connects to localhost:27017
+
+    $manager = new MongoDB\Driver\Manager("mongodb://root:root_pass@mongo_db_data_storage:27017");
+
+   /* Query for all the items in the collection */
+    $query = new MongoDB\Driver\Query( [] );
+
+    /* Query the "asteroids" collection of the "test" database */
+    $cursor = $manager->executeQuery("cinema_db.Cinemas", $query);
+
+    /* $cursor now contains an object that wraps around the result set. Use
+    * foreach() to iterate over all the result */
+    foreach($cursor as $document) {
+        echo $document->name."<br>";
+    }
+
+
+
+
 
 ?>
