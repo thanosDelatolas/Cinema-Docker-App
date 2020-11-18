6e96e1ec75ef10b5985e84295c689e64fe2ded96
thanosdel <thanos.delatolas98@gmail.com>
2020-11-18 19:07:03 +0200

    orion

--- Data-Storage/index.php
+++ Data-Storage/index.php
@@ -1,25 +1,27 @@
 <?php
-   require 'mongo_conn.php';
    
-   $cin_id = "5fac44e1b711b9d9961da7b1";
+   $curl = curl_init();
 
-   //get all Movies playing in this cinema
-   $filter = [
-      'playing_in' => $cin_id
-   ];
-   $options = [ 'projection' => ['_id' => 1]];
-   
-   //get name and id of last movie bought!
-   $query = new \MongoDB\Driver\Query($filter, $options);
-   $movies  = $manager->executeQuery('cinema_db.Movies', $query);
-   $movies_array = $movies->toArray();
-   
-   //Delete all the favorites!
-   foreach($movies_array as $movie){
-      echo (string)$movie->_id."<br>";
+   curl_setopt_array($curl, array(
+   CURLOPT_URL => "http://orion:1026/v2/entities?options=keyValues",
+   CURLOPT_RETURNTRANSFER => true,
+   CURLOPT_ENCODING => "",
+   CURLOPT_MAXREDIRS => 10,
+   CURLOPT_TIMEOUT => 0,
+   CURLOPT_FOLLOWLOCATION => true,
+   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+   CURLOPT_CUSTOMREQUEST => "POST",
+   CURLOPT_POSTFIELDS =>"{\n    \"id\": \"5fad031a8ef6a0295698d829\",\n    \"type\": \"Movie\",\n    \"start_date\": \"2021-01-16\",\n    \"end_date\": \"2021-03-30\"\n}",
+   CURLOPT_HTTPHEADER => array(
+      "Content-Type: application/json",
+      "Cookie: PHPSESSID=2443011ea8c096956d831711254ca07f; pma_lang=en; phpMyAdmin=ec08d3bd45eff69b3c11e406ec59bcc5"
+   ),
+   ));
 
-   }
+   $response = curl_exec($curl);
 
+   curl_close($curl);
+   echo $response;
   
 
 
