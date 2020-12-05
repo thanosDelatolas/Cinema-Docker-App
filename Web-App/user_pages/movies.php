7fc61309c44d25ef5b47ff878e48db87a5a5368b
thanosdel <thanos.delatolas98@gmail.com>
2020-12-05 12:52:37 +0200

    gcp..

--- Web-App/user_pages/movies.php
+++ Web-App/user_pages/movies.php
@@ -16,8 +16,11 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+
+    <!-- maybe for gcp....... -->
     <script src="https://apis.google.com/js/client.js"></script>
+    
     <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon.ico">
     <title>Movies</title>
 </head>
