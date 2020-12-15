5ade5d0b47919b97bcf4a64f4ab80d8f1e59f146
thanosDelatolas <thanos.delatolas98@gmail.com>
2020-12-15 20:17:38 +0200

    FINISHED

--- Web-App/app_js/global.js
+++ Web-App/app_js/global.js
@@ -1,8 +1,8 @@
 //when u want to run the project online
-var app_logic_ip="http://34.65.60.180:81/app_logic_req.php";
+//var app_logic_ip="http://34.65.60.180:81/app_logic_req.php";
 
 //when you want to run the project locally
-//var app_logic_ip = "http://172.18.1.8/app_logic_req.php";
+var app_logic_ip = "http://172.18.1.8/app_logic_req.php";
 
 /**
  * Either line 2 or line 5 must be in comments depending on the mode u will run the project
