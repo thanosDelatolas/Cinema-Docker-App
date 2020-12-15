6b369ffa6c69e99865dd85da0cda4ec125b31525
thanosDelatolas <thanos.delatolas98@gmail.com>
2020-12-15 17:23:13 +0200

    gcp

--- Web-App/app_js/movies_jquery.js
+++ Web-App/app_js/movies_jquery.js
@@ -5,7 +5,7 @@
 $(document).ready(function(){
     
     //*********** jquery for movies.php **********************
-    var application_logic_url = "http://172.18.1.8:80/app_logic_req.php";
+    var application_logic_url = "http://34.65.60.180:80/app_logic_req.php";
 
         
     //for all buttons with id=fav_1,fav_2,...etc
